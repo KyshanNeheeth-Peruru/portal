f527a7e65ed54d0cc70f3b586a28e67847654178
KyshanNeheeth-Peruru <kishan.nehith@gmail.com>
2023-07-20 16:56:13 -0400

    updated lab_list page

--- apply/views.py
+++ apply/views.py
@@ -18,6 +18,7 @@ from apply.utils import token_generator
 from apply.ldap_helper import LDAPHelper
 from apply.remote_connect import RemoteConnect
 from apply.ldap import LDAP
+from django.db.models import Q
 
 # Create your views here.
 
@@ -234,13 +235,13 @@ def faq(request):
     return render(request, "../templates/faq.html",{'faqs':faqs})
 
 def lablist(request):
-    sems=Semesters.objects.all()
+    sems=reversed(Semesters.objects.all())
     if request.method == 'POST':
         selected_sem = request.POST["selected_sem"]
-        # allusers=(User.objects.filter(usercourses__course__course_semester__semester_longname=selected_sem,groups__name='Faculty').distinct().order_by('last_name'))
-        non_faculty = User.objects.filter(usercourses__course__course_semester__semester_longname=selected_sem).exclude(groups__name='Faculty').distinct().order_by('last_name')
-        faculty=(User.objects.filter(usercourses__course__course_semester__semester_longname=selected_sem,groups__name='Faculty').distinct().order_by('last_name'))
-        return render(request, "../templates/lab_list.html",{'sems':sems,'selected_sem':selected_sem,'listFac':faculty, 'listNonFac':non_faculty})
+        allusers = User.objects.filter(Q(usercourses__course__course_semester__semester_longname=selected_sem) | Q(is_staff=True)).order_by('last_name')
+        # non_faculty = User.objects.filter(usercourses__course__course_semester__semester_longname=selected_sem).exclude(groups__name='Faculty').distinct().order_by('last_name')
+        # faculty=(User.objects.filter(usercourses__course__course_semester__semester_longname=selected_sem,groups__name='Faculty').distinct().order_by('last_name'))
+        return render(request, "../templates/lab_list.html",{'sems':sems,'selected_sem':selected_sem,'lab_list':allusers})
     return render(request, "../templates/lab_list.html",{'sems':sems})
 
 
