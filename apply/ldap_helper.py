2022750f00a5fb6b6ea74ba9aa0f9bb80dbc7152
KyshanNeheeth-Peruru <kishan.nehith@gmail.com>
2023-08-29 14:13:54 -0400

    test

--- apply/ldap_helper.py
+++ apply/ldap_helper.py
@@ -166,7 +166,7 @@ class LDAPHelper:
                                        "(&(objectClass=person)(sAMAccountName=" + str(self.userName) + "))",
                                        attributes=['cn'])
                 print("results:")
-                print(results[dn])
+                print(results[0])
                 if results:
                     return ldap_conn.entries[0].cn[0]
                 else:
