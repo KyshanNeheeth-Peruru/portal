281c7955e7271c30797793e4d20bf33d0dbc47df
KyshanNeheeth-Peruru <kishan.nehith@gmail.com>
2023-09-19 15:38:33 -0400

    register

--- apply/views.py
+++ apply/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirectget_current_site
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
