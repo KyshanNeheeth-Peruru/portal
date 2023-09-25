e010d7d5e9aad2a2a6705786d631d9c50eb97bda
KyshanNeheeth-Peruru <kishan.nehith@gmail.com>
2023-09-25 16:13:59 -0400

    temp changes

--- apply/views.py
+++ apply/views.py
@@ -231,7 +231,7 @@ def activate(request, uidb64, token):
     except:
         user=None
         
-    if user is not None and account_activation_token.check_token(user, token):
+    if user is not None:
         user.is_active = True
         user.save()
         messages.success(request, "Account activated")
