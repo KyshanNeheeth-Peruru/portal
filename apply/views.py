eccce0dd8497283a18d2eeefbbe3e0c4a856faad
KyshanNeheeth-Peruru <kishan.nehith@gmail.com>
2023-10-16 15:00:17 -0400

    pasw

--- apply/views.py
+++ apply/views.py
@@ -145,6 +145,21 @@ def recover_password(request, uidb64, token):
     try:
         uid = force_text(urlsafe_base64_decode(uidb64))
         user = get_user_model().objects.get(pk=uid)
+        
+        if default_token_generator.check_token(user, token):
+            # Here, you should render a form for the user to set a new password
+            # After setting the new password, you can log the user in and redirect to a success page
+            # You may also want to add validation to the password setting process
+
+            if request.method == 'POST':
+                new_password = request.POST.get('new_password')
+                user.set_password(new_password)
+                user.save()
+                login(request, user)  # Log the user in with the new password
+                messages.success(request, "Password reset successfully.")
+                return redirect('login')  # Redirect to the login page or a success page
+
+            return render(request, 'reset_password_form.html')
     except:
         user=None
     if user is not None:
@@ -311,65 +326,6 @@ def logout_view(request):
     messages.success(request,"Logged out")
     return redirect('login')
 
-
-# @login_required(login_url="/accounts/login/")
-# def selected_courses(request):
-#     if request.method == "POST":
-#         userID = request.user.id
-#         selectedID = request.POST.getlist("id")
-#         userName = request.user.username
-#         # obj = LDAPHelper(**{"userName": userName})
-#         # remote_connection = RemoteConnect()
-#         semester = semester_year().lower().split()
-#         if semester[0] == "summer":
-#             semester = semester[0][:3] + semester[1][2:]
-#         else:
-#             semester = semester[0][:1] + semester[1][2:]
-
-#         for courseID in selectedID:
-#             temp = Courses.objects.filter(id=int(courseID))
-#             selectedCourse = temp[0].course_number
-#             selectedCourseSection = temp[0].course_section
-#             selectedCoursesInstructor = temp[0].course_instructor
-#             selectedCoursesSemester = temp[0].course_semester.semester_abbrev
-#             prof_unix_name = temp[0].prof_unix_name
-#             insert_courses_into_user_courses(userID,
-#                                              selectedCourse,
-#                                              selectedCoursesSemester,
-#                                              selectedCourseSection,
-#                                              selectedCoursesInstructor
-#                                              )
-#             ldapCourseSection = f"{selectedCourse}-{selectedCourseSection}"
-#             graderGroup = f"{selectedCourse}-{selectedCourseSection}G"
-#             uid = obj.get_uid_number()
-#             obj.add_user_to_courses(ldapCourseSection)
-#             remote_connection.execute_command(f"sudo python3 /srv/course_directory.py -user {userName} "
-#                                               f"-course {selectedCourse} -sem {semester} "
-#                                               f"-prof {prof_unix_name} -uid {uid} -graderGroup {graderGroup}")
-
-#         return HttpResponseRedirect("registered/")
-#     else:
-#         current_semester = semester_year()
-#         # Get a Queryset of registered Courses
-#         user_registered_courses = UserCourses.objects.filter(semester_year=current_semester,
-#                                                              user_id=request.user.id).order_by("course")
-#         courses = []
-#         try:
-#             # exclude the courses from the display list which the user has already registered
-#             course_semester_id = Semesters.objects.filter(semester_longname=current_semester).values_list("semester_abbrev")[0]
-#             courses = Courses.objects.exclude(
-#                 course_number__in=[c.course for c in user_registered_courses]).filter(
-#                 course_semester_id=course_semester_id).order_by("course_number")
-#         except Exception as ex:
-#             logger.error("Exception: ", ex)
-#         if courses is None or len(courses) == 0:
-#             courses = []
-#         return render(
-#             request,
-#             "../templates/courses.html",
-#             {"courses": courses, "current_semester": current_semester},
-#         )
-
 @login_required
 def selected_courses(request):
     current_semester = Semesters.objects.filter(is_active=True).first()
@@ -482,21 +438,6 @@ def change_password(request):
             messages.error(request,'Passwords dont match.')
             return render(request, '../templates/registration/change_password.html')
     return render(request, '../templates/registration/change_password.html')
-        
-    #     if form.is_valid():
-    #         user = request.user.username
-    #         password = request.POST["new_password1"]
-    #         #change_ldap_password(user, password)
-    #         user = form.save()
-    #         update_session_auth_hash(request, user)  # Important! (To keep the user logged in)
-    #         # messages.success(request, 'Your password was successfully updated!')
-    #         return render(request, '../templates/registration/change_password_complete.html')
-    #     else:
-    #         messages.error(request, 'Please correct the error below.')
-    # else:
-    #     form = SetPasswordForm(request.user)
-    # return render(request, '../templates/registration/change_password.html', {'form': form})
-
     
 def about_us(request):
     return render(request, "../templates/about_us.html")
