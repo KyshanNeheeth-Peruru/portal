26b79b14f10dfb8a784de18deed50155fc22d19f
KyshanNeheeth-Peruru <kishan.nehith@gmail.com>
2023-11-10 01:33:41 -0500

    find username

--- apply/views.py
+++ apply/views.py
@@ -522,7 +522,7 @@ def check_username(request):
                 try:
                     user = User.objects.get(email=email)
                     username = user.username
-                    return JsonResponse(username)
+                    return JsonResponse({'username': username})
                 except User.DoesNotExist:
                     return JsonResponse({'message': 'Email does not exist'}, status=404)
             else:
